737fa00303a997cf6733515311e8ec25708c96e3
Byte Me <chemerilcollins@gmail.com>
2024-04-02 02:42:51 +0200

    Refined file structure and automation of file compilation/running

---
+++ dist/types.js
@@ -0,0 +1,20 @@
+"use strict";
+// *Declaring variables with types
+let age0 = 22; // Here age is a number. Now we cannot assign any other type to age.
+let isMale = true; // Here isMale is a boolean. Now we cannot assign any other type to isMale.
+let student = "John"; // Here name is a string. Now we cannot assign any other type to name.
+// *Declaring variables without types(Type Inference)
+let age2 = 22; // Here age is a number. Now we cannot assign any other type to age.
+let isMale2 = false; // Here isMale is a boolean. Now we cannot assign any other type to isMale.
+let student2 = "Doe"; // Here name is a string. Now we cannot assign any other type to name.
+age2 = 23; // Error: Type 'string' is not assignable to type 'number'.
+//* Declaring variables with multiple types
+/*Note that the latest value assigned to the variable will be the type of the variable.
+*/
+let variable;
+variable = true;
+console.log(variable); // true
+variable = "Hello";
+console.log(variable); // Hello
+variable = 22;
+console.log(variable); // 22
--- test.js
+++
@@ -1,5 +0,0 @@
-console.log('2'+ 2);
-let user = {name: 'John', email: 'john@email.com'};
-// let {name, email} = user; // destructuring
-let age = user.age;
-console.log(age); // undefined
